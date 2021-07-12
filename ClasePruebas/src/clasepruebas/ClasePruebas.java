890d8983bf54d7aaad5af702d49d533a0eff8e7c
user <user@DESKTOP-EOQAJOP>
2021-07-12 09:23:04 -0500

    segundo cambio

--- ClasePruebas/src/clasepruebas/ClasePruebas.java
+++ ClasePruebas/src/clasepruebas/ClasePruebas.java
@@ -5,6 +5,8 @@
  */
 package clasepruebas;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author user
@@ -15,7 +17,7 @@ public class ClasePruebas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        JOptionPane.showMessageDialog(null,"Holaaa");
     }
     
 }
